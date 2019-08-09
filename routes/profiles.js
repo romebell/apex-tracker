3c08e05eb4b99b8d4dcb71c83b09d558355b4a79
Rome Bell <romebell@me.com>
2019-08-08 23:00:00 -0700

    Update route and add profile routes to server

--- routes/profiles.js
+++ routes/profiles.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // route
-router.get('/api/v1/profile/:platform/:gamertag', (req, res) => {
+router.get('/:platform/:gamertag', (req, res) => {
   console.log(req.params.platform, req.params.gamertag);
   res.send('Apex Tracker');
 });
--- server.js
+++ server.js
@@ -8,6 +8,9 @@ dotenv.config({ path: './config.env'});
 const app = express();
 const port = process.env.PORT || 8000;
 
+// Profile routes
+app.use('/api/v1/profile', require('./routes/profiles'));
+
 // look at env variable
 app.listen(port, () => {
   console.log(`Apex Legends Tracker: SERVER ON \n - ${process.env.NODE_ENV} mode on port ${port}`);
