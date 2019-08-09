28581a909a1492f62ddb3dad8f30e8d0ee60791c
Rome Bell <romebell@me.com>
2019-08-08 22:27:42 -0700

    Move route from server to profile.js

--- server.js
+++ server.js
@@ -8,12 +8,6 @@ dotenv.config({ path: './config.env'});
 const app = express();
 const port = process.env.PORT || 8000;
 
-// route
-app.get('/api/v1/profile/:platform/:gamertag', (req, res) => {
-  console.log(req.params.platform, req.params.gamertag);
-  res.send('Apex Tracker');
-})
-
 // look at env variable
 app.listen(port, () => {
   console.log(`Apex Legends Tracker: SERVER ON \n - ${process.env.NODE_ENV} mode on port ${port}`);
