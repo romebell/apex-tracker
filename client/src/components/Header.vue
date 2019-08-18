aa4e334507c972ba1beca607c23696d05c5ec37a
Rome Bell <romebell@me.com>
2019-08-18 16:57:45 -0700

    Name component and pass in image

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,10 +1,17 @@
 <template>
-  
+  <header>
+    <img :src="image">
+  </header>
 </template>
 
 <script>
 export default {
-
+  name: 'Header',
+  data() {
+    return {
+      image: require('@/assets/logo.png')
+    }
+  }
 }
 </script>
 
