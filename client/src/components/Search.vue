fd97839c53c8b97fdb50e7ac0259309120880621
Rome Bell <romebell@me.com>
2019-08-18 17:42:44 -0700

    Add Platform Gamertag and button to search component

--- client/src/components/Search.vue
+++ client/src/components/Search.vue
@@ -1,5 +1,30 @@
 <template>
-  
+  <section class="search">
+    <h1>Track Player Stats</h1>
+    <form v-on:submit.prevent="onSubmit">
+      <div class="form-group">
+        <label for="platform">Platform</label>
+        <select name="platform" id="platform" v-model="platform">
+          <option value="psn">Playstation</option>
+          <option value="xbl">Xbox</option>
+          <option value="origin">Origin</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="gamertag">Gamertag</label>
+        <input 
+        type="text" 
+        name="text" 
+        v-model="gamertag" 
+        id="gamertag" 
+        placeholder="Origin ID, Xbox Live gamertag, PSN, ID, etc"
+        />
+      </div>
+      <div class="form-group">
+        <input type="submit" value="Submit">
+      </div>
+    </form>
+  </section>
 </template>
 
 <script>
