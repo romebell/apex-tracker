3401ba2a344bbe7524e4c143580520cea4be0771
Rome Bell <romebell@me.com>
2019-08-18 20:12:53 -0700

    Add Profile component with data() and beforeCreate()

--- client/src/components/Profile.vue
+++ client/src/components/Profile.vue
@@ -6,7 +6,17 @@
 
 <script>
 export default {
-
+  name: 'Profile',
+  data() {
+    return {
+      loading: false,
+      error: null,
+      profileData: null
+    }
+  },
+  beforeCreate() {
+    document.body.className = 'body-bg-no-image';
+  }
 }
 </script>
 
