ea1813294ce99cf66fbb795a1e681cce1a790a01
Rome Bell <romebell@me.com>
2019-08-18 22:29:48 -0700

    Handle production set static folder and handle spa

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ if (process.env.NODE_ENV === 'development') {
 // Profile routes
 app.use('/api/v1/profile', require('./routes/profiles'));
 
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static(__dirname + '/public/'));
+
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 // look at env variable
 app.listen(port, () => {
   console.log(`Apex Legends Tracker: SERVER ON \n - ${process.env.NODE_ENV} mode on port ${port}`);
