2a86758cb3cd8169ccb2869ea96b0364a389b1bf
Rome Bell <romebell@me.com>
2019-08-18 20:40:30 -0700

    Add created()for getting response

--- client/src/components/Profile.vue
+++ client/src/components/Profile.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: 'Profile',
   data() {
@@ -16,6 +17,19 @@ export default {
   },
   beforeCreate() {
     document.body.className = 'body-bg-no-image';
+  },
+  async created() {
+    this.loading = true;
+    try {
+      const response = await axios.get(`http://localhost:5000/api/v1/profile/
+        ${this.$route.params.platform}/${this.$route.params.gamertag}`);
+        this.profileData = response.data.data;
+        // console.log(this.profileData);
+        this.loading = false;
+    } catch (err) {
+      this.loading = false;
+      this.error = err.response.data.message;
+    }
   }
 }
 </script>
